8bb142f4ff7fe79adc785020aff1d594574681c9
Matthew Sandoz <msandoz@users.sourceforge.net>
2010-03-03 22:41:22 -0500

    adding eclipse basic artifacts to main maven build

---
+++ nexusbpm/droolsflow/designer/src/main/java/org/nexusbpm/droolsflow/designer/App.java
@@ -0,0 +1,13 @@
+package org.nexusbpm.droolsflow.designer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
