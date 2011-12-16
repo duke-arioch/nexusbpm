cc14d1b24c68fdda90c43b60321baf4bdaf79132
Matthew Sandoz <matthew.sandoz@gmail.com>
2011-12-16 00:27:30 -0500

    finally got a failing test

--- activiti/service/src/main/java/org/nexusbpm/activiti/delegate/ExcelNexusJavaDelegate.java
+++ activiti/service/src/main/java/org/nexusbpm/activiti/delegate/ExcelNexusJavaDelegate.java
@@ -1,5 +1,6 @@
 package org.nexusbpm.activiti.delegate;
 
+import java.net.URI;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,6 +8,7 @@ import java.util.Map.Entry;
 
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
+import org.activiti.engine.impl.el.Expression;
 import org.apache.commons.beanutils.BeanUtils;
 import org.nexusbpm.service.NexusServiceResponse;
 import org.nexusbpm.service.excel.ExcelServiceImpl;
@@ -17,7 +19,12 @@ public class ExcelNexusJavaDelegate extends ExcelServiceImpl implements
 
 	public static final Map<String, String> INPUT_MAP, OUTPUT_MAP;
 	
-  private Boolean skipHeader;
+  private Expression skipHeader;
+  private Expression rowLimit;
+  private Expression columnLimit;
+  private Expression excelAnchor;
+  private Expression sheetName;
+  private Expression dataFile;
   
 	static {
 		Map<String, String> inMap = new HashMap<String, String>();
@@ -44,20 +51,14 @@ public class ExcelNexusJavaDelegate extends ExcelServiceImpl implements
 				BeanUtils.setProperty(request, entry.getKey(), vars.get(entry.getValue()));			
 			}
 		}
+    request.setDataFile(new URI("res://activiti.cfg.xml"));
+    request.setTemplateFile(new URI("res://activiti.cfg.xml"));
+    request.setOutputFile(new URI("res://test.xls"));
 		NexusServiceResponse response = super.execute(request);
 		for (Entry<String, String> entry : OUTPUT_MAP.entrySet()) {
 			Object output = BeanUtils.getProperty(response, entry.getKey());
 			execution.setVariable(entry.getValue(), output);
 		}
 	}
-
-  public void setSkipHeader(Boolean skipHeader) {
-    this.skipHeader = skipHeader;
-  }
-
-  public Boolean getSkipHeader() {
-    return skipHeader;
-  }
-
 
 }
