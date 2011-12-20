3fddf75fc6badb6f4a14abe362b02a7960f32bd6
Matthew Sandoz <matthew.sandoz@gmail.com>
2011-12-19 20:54:59 -0500

    new delegate added with ability to be configured via spring and evaluate expressions

--- activiti/service/src/main/java/org/nexusbpm/activiti/delegate/ExcelNexusJavaDelegate.java
+++ activiti/service/src/main/java/org/nexusbpm/activiti/delegate/ExcelNexusJavaDelegate.java
@@ -1,64 +1,38 @@
 package org.nexusbpm.activiti.delegate;
 
-import java.net.URI;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.JavaDelegate;
+import org.activiti.engine.impl.context.Context;
 import org.activiti.engine.impl.el.Expression;
-import org.apache.commons.beanutils.BeanUtils;
-import org.nexusbpm.service.NexusServiceResponse;
 import org.nexusbpm.service.excel.ExcelServiceImpl;
 import org.nexusbpm.service.excel.ExcelServiceRequest;
+import org.nexusbpm.service.excel.ExcelServiceResponse;
 
-public class ExcelNexusJavaDelegate extends ExcelServiceImpl implements
-		JavaDelegate {
+public class ExcelNexusJavaDelegate extends NexusJavaDelegate implements
+        JavaDelegate {
 
-	public static final Map<String, String> INPUT_MAP, OUTPUT_MAP;
-	
   private Expression skipHeader;
   private Expression rowLimit;
   private Expression columnLimit;
   private Expression excelAnchor;
   private Expression sheetName;
   private Expression dataFile;
-  
-	static {
-		Map<String, String> inMap = new HashMap<String, String>();
-		inMap.put("skipHeader", "skipHeader");
-		inMap.put("rowLimit", "rowLimit");
-		inMap.put("columnLimit", "columnLimit");
-		inMap.put("excelAnchor", "excelAnchor");
-		inMap.put("sheetName", "templateFile");
-		inMap.put("dataFile", "dataFile");
-		INPUT_MAP = Collections.unmodifiableMap(inMap);
+  private Expression templateFile;
+  private Expression outputFile;
 
-		Map<String, String> outMap = new HashMap<String, String>();
-		outMap.put("outputFile", "outputFileVariableName");
-		outMap.put("returnCode", "status");
-		OUTPUT_MAP = Collections.unmodifiableMap(outMap);
-	}
+  @Override
+  public void execute(DelegateExecution execution) throws Exception {
+    ExcelServiceImpl impl = (ExcelServiceImpl) Context.getProcessEngineConfiguration().getBeans().get("excelService");
 
-	@Override
-	public void execute(DelegateExecution execution) throws Exception {
-		ExcelServiceRequest request = new ExcelServiceRequest();
-		Map<String, Object> vars = execution.getVariables();
-		for (Entry<String, String> entry : INPUT_MAP.entrySet()) {
-			if (vars.containsKey(entry.getValue())) {
-				BeanUtils.setProperty(request, entry.getKey(), vars.get(entry.getValue()));			
-			}
-		}
-    request.setDataFile(new URI("res://activiti.cfg.xml"));
-    request.setTemplateFile(new URI("res://activiti.cfg.xml"));
-    request.setOutputFile(new URI("res://test.xls"));
-		NexusServiceResponse response = super.execute(request);
-		for (Entry<String, String> entry : OUTPUT_MAP.entrySet()) {
-			Object output = BeanUtils.getProperty(response, entry.getKey());
-			execution.setVariable(entry.getValue(), output);
-		}
-	}
-
+    ExcelServiceRequest request = new ExcelServiceRequest();
+    request.setSkipHeader(evaluateToBoolean(skipHeader, execution));
+    request.setRowLimit(evaluateToInt(rowLimit, execution));
+    request.setColumnLimit(evaluateToInt(columnLimit, execution));
+    request.setExcelAnchor(evaluateToString(excelAnchor, execution));
+    request.setSheetName(evaluateToString(sheetName, execution));
+    request.setDataFile(evaluateToUri(dataFile, execution));
+    request.setOutputFile(evaluateToUri(outputFile, execution));
+    request.setTemplateFile(evaluateToUri(templateFile, execution));
+    ExcelServiceResponse response = (ExcelServiceResponse) impl.execute(request);
+  }
 }
--- activiti/service/src/main/java/org/nexusbpm/activiti/delegate/NexusJavaDelegate.java
+++ activiti/service/src/main/java/org/nexusbpm/activiti/delegate/NexusJavaDelegate.java
@@ -1,13 +1,30 @@
 package org.nexusbpm.activiti.delegate;
 
+import java.net.URI;
+
 import org.activiti.engine.delegate.DelegateExecution;
-import org.activiti.engine.delegate.JavaDelegate;
+import org.activiti.engine.impl.context.Context;
+import org.activiti.engine.impl.el.Expression;
 
-public class NexusJavaDelegate implements JavaDelegate {
+public class NexusJavaDelegate {
 
-	@Override
-	public void execute(DelegateExecution execution) throws Exception {
-		
-	}
+  protected String evaluateToString(Expression expression, DelegateExecution execution) {
+    return evaluate(expression, execution).toString();
+  }
 
+  protected Boolean evaluateToBoolean(Expression expression, DelegateExecution execution) {
+    return Boolean.valueOf(evaluate(expression, execution).toString());
+  }
+
+  protected Integer evaluateToInt(Expression expression, DelegateExecution execution) {
+    return Integer.valueOf(evaluate(expression, execution).toString());
+  }
+
+  protected URI evaluateToUri(Expression expression, DelegateExecution execution) {
+    return URI.create(evaluate(expression, execution).toString());
+  }
+
+  protected Object evaluate(Expression expression, DelegateExecution execution) {
+    return Context.getProcessEngineConfiguration().getExpressionManager().createExpression(expression.getExpressionText()).getValue(execution);
+  }
 }
